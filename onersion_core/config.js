e9d1fefcc1843409bda9626c354a28e2ba5c83a2
Brandon Marsh <bm801643@gmail.com>
2025-08-14 10:30:42 -0500

    Create config.js

---
+++ onersion_core/config.js
@@ -0,0 +1,7 @@
+// Onersion Core Configuration Loader
+const systemConfig = {
+    glyphodesPath: "glyphodes.json",
+    sigilPath: "sigil.json",
+    weavePath: "weave.json",
+    convergenceManifestPath: "convergence_manifest.json"
+};
